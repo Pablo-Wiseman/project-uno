639a60ff6bb22557370d23227084df0bc33f6c6c
Your Name <you@example.com>
2022-07-13 14:11:38 +0500

    body lock initialized

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
-//*burger
+//*burger кнопка + меню
 
 const burgerBtn = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.nav');
+const scrollLock = document.querySelector('.body-page');
 
 burgerBtn.addEventListener('click', function () {
     burgerBtn.classList.toggle('burger--active');
     burgerMenu.classList.toggle('nav--active');
+    scrollLock.classList.toggle('scroll-lock');
 });
