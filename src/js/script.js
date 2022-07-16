9ff3b5a44dec5845ad07916fbf1f06f3a7e85784
Your Name <you@example.com>
2022-07-16 17:53:43 +0500

    search updated

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,8 @@ burgerBtn.addEventListener('click', function () {
 const searchBtn = document.querySelector('.header__btn');
 const searchField = document.querySelector('.search');
 
+
 searchBtn.addEventListener('click', function () {
     searchField.classList.toggle('search--active');
+    searchBtn.classList.toggle('header__btn--active');
 });
