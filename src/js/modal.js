d1a0452f061aa26dd79682ce56ec0ba8141a2ecb
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2022-07-26 01:46:34 +0500

    plagins updated

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,17 +5,15 @@ const modalGallery = document.querySelector('.gallery-modal');
 const body = document.body;
 
 //? add click to each button
-document.querySelectorAll('.gallery-swiper__slide').forEach((btnOpen) => { 
+document.querySelectorAll('.gallery-swiper__slide').forEach((btnOpen) => {
     btnOpen.addEventListener('click', function () {
-    modalGallery.classList.add('gallery-modal--active');
-    body.classList.add('scroll-lock');
-
+        modalGallery.classList.add('gallery-modal--active');
+        body.classList.add('scroll-lock');
     });
 });
 
 //? remove page scroll-lock class and hide modal window
-btnClose.addEventListener('click',  () => {
+btnClose.addEventListener('click', () => {
     document.body.classList.remove('scroll-lock');
     modalGallery.classList.remove('gallery-modal--active');
-    
 });
