16af5944ae13b6a45bf87d0d6d65b53131dc1737
Your Name <you@example.com>
2022-07-26 22:22:05 +0500

    accordion step 1

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const accordions = document.querySelectorAll('.accordion__item');
-  for (item of accordions) {
-    item.addEventListener('click', function () {
-      if (this.classList.contains('active')) {
-        this.classList.remove('active');
-      } else {
-        for (el of accordions) {
-          el.classList.remove('active');
-        }
-        this.classList.add('active');
-      }
-    });
-  }
-
-
-  
-});
+new Accordion('.accordion-container');
