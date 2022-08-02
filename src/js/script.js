97ce00c8ccb27889312be862540c31baac22b4a4
Your Name <you@example.com>
2022-08-02 19:13:16 +0500

    +

--- src/js/script.js
+++ src/js/script.js
@@ -16,12 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchBtn = document.querySelector('.header__btn');
   const searchField = document.querySelector('.search');
 
+
   searchBtn.addEventListener('click', function () {
     searchField.classList.toggle('search--active');
     searchBtn.classList.toggle('header__btn--active');
   });
 
-  //*tabs переключение жанров
+
+  // //*tabs переключение жанров
 
   document.querySelectorAll('.subheader__btn').forEach(function (tabsBtn) {
     tabsBtn.addEventListener('click', function (event) {
@@ -30,18 +32,25 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelectorAll('.subheader__btn').forEach(function (btn) {
         btn.classList.remove('subheader__btn--active');
         event.currentTarget.classList.add('subheader__btn--active');
+        if (event.currentTarget.classList.contains('subheader__btn--active')) {
+          btn.classList.remove('subheader__btn--active');
+        } else { 
+          event.currentTarget.classList.add('subheader__btn--active');
+        }
       });
 
-      document
-        .querySelectorAll('.subheader__content')
-        .forEach(function (tabsBtn) {
+      
+        document.querySelectorAll('.subheader__content').forEach(function (tabsBtn) {
           tabsBtn.classList.remove('subheader__content--active');
         });
-      document
-        .querySelector(`[data-target="${path}"]`)
-        .classList.add('subheader__content--active');
+        
+      
+        document.querySelector(`[data-target="${path}"]`).classList.toggle('subheader__content--active');
     });
+        
   });
+  
+  
 
   
 
